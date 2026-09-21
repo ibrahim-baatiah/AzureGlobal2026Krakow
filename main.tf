3a7a537dd44a12118be7375be327d96e913150d5
Ibrahim <d.homy.2010@hotmail.com>
2026-09-21 17:41:46 +0200

    Adding the Database Connection

--- main.tf
+++ main.tf
@@ -47,6 +47,14 @@ module "mssql_server" {
 
   sql_server_version = "12.0"
 
+    databases = [{
+    name                 = "moviesdb"
+    size                 = "2"
+    sku                  = "Basic"
+    storage_account_type = "Local"
+    collation            = "SQL_Latin1_General_CP1_CI_AS"
+  }]
+
 }
 
 module "application_insights" {
@@ -97,6 +105,13 @@ module "app_service" {
   app_settings = {
     "ApplicationInsights__ConnectionString" = module.application_insights.connection_string
     "WEBSITES_PORT" = "8080"
+    "ConnectionStrings__RazorPagesMovieContext" = join("", [
+      "Server=tcp:${module.mssql_server.server.fully_qualified_domain_name},1433;",
+      "Initial Catalog=moviesdb;",
+      "User ID=${module.mssql_server.server.administrator_login};",
+      "Password=${module.mssql_server.server.administrator_login_password};",
+      "Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
+    ])
   }
   identity_client_id = module.managed_identity.managed_identity_client_id
   identity_id = module.managed_identity.managed_identity_id
